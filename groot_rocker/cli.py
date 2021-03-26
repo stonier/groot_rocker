bb99593ad0710d83bb5cfb41232bdc3eeedcd173
Daniel Stonier <d.stonier@gmail.com>
2021-03-26 12:50:52 -0400

    [cli] preloading defaults from yaml (#20)
    Resolves #2

--- groot_rocker/cli.py
+++ groot_rocker/cli.py
@@ -15,6 +15,8 @@ The command line client.
 ##############################################################################
 
 import argparse
+import typing
+import yaml
 
 from . import console
 from . import core
@@ -26,57 +28,106 @@ from . import version
 ##############################################################################
 
 
-def main():
+def set_default(option: str, yaml_defaults: str) -> typing.Any:
+    return yaml_defaults[option] if option in yaml_defaults else None
+
+##############################################################################
+# Main
+##############################################################################
+
+
+def load_arguments(
+    command_line_arguments: typing.Optional[typing.List[str]]=None
+) -> typing.Dict[str, typing.Any]:
+    config_parser = argparse.ArgumentParser(add_help=False)
+    config_parser.add_argument(
+        '-c', '--config', type=str, metavar="YAML", default=None, help="pre-load options from a yaml file"
+    )
+    config_args, remaining_argv = config_parser.parse_known_args(command_line_arguments)
+
+    yaml_defaults = {}
+    if config_args.config is not None:
+        with open(config_args.config, 'r') as stream:
+            yaml_defaults = yaml.load(stream)
 
     parser = argparse.ArgumentParser(
-        description='A tool for running docker with extra options'
+        parents=[config_parser],
+        description='A tool for running docker with extra options',
+        # formatter_class=argparse.RawDescriptionHelpFormatter,
     )
     parser.add_argument(
         '-v', '--version', action='version', version='%(prog)s ' + version.__version__
     )
 
     image_options = parser.add_argument_group(title="Image Options", description="These options are transferred to 'docker image build'.")
-    image_options.add_argument('--nocache', action='store_true', help="do not use cache when building")
-    image_options.add_argument('--pull', action='store_true', help="always attempt to pull newer versions")
-    image_options.add_argument('--tag', type=str, default=None, help="image identifiers in the form name:tag")
+    image_options.add_argument(
+        '--nocache', action='store_true',
+        default=set_default("nocache", yaml_defaults),
+        help="do not use cache when building"
+    )
+    image_options.add_argument(
+        '--pull', action='store_true',
+        default=set_default("pull", yaml_defaults),
+        help="always attempt to pull newer versions"
+    )
+    image_options.add_argument(
+        '--tag', type=str,
+        default=set_default("tag", yaml_defaults),
+        help="image identifiers in the form name:tag"
+    )
 
     run_options = parser.add_argument_group(title="Run Options", description="These options are transferred to 'docker run'.")
-    run_options.add_argument('--persistent', action='store_true', help="persist the container post-execution")
+    run_options.add_argument(
+        '--persistent', action='store_true',
+        default=set_default("persistent", yaml_defaults), help="persist the container post-execution"
+    )
 
-    parser.add_argument('image')
-    parser.add_argument('command', nargs='*', default='')
+    parser.add_argument(
+        'image', nargs='?',
+        default=set_default("image", yaml_defaults),
+        help="base image to build from (required)"
+    )
+    default_command = yaml_defaults["command"] if "command" in yaml_defaults else []
+    if isinstance(default_command, str):  # user convenience
+        default_command = default_command.split(' ')
+    parser.add_argument('command', nargs='*', default=default_command, help="command to run upon entering the container")
 
     extensions = parser.add_argument_group(title="Extensions")
 
     try:
         extension_manager = core.RockerExtensionManager()
-        default_args = {}
-        extension_manager.extend_cli_parser(extensions, default_args)
+        extension_manager.extend_cli_parser(extensions, yaml_defaults)
     except core.DependencyMissing as ex:
         # Catch errors if docker is missing or inaccessible.
         parser.error("DependencyMissing encountered: %s" % ex)
 
-    args = parser.parse_args()
-    args_dict = vars(args)
+    args = vars(parser.parse_args(remaining_argv))  # work with a dict object from here, not argparse.Namespace
+    args["command"] = ' '.join(args["command"])  # Convert command into string
 
-    active_extensions = extension_manager.get_active_extensions(args_dict)
+    return args
+
+
+def main():
+    options = load_arguments()
+    extension_manager = core.RockerExtensionManager()
+    active_extensions = extension_manager.get_active_extensions(options)
     # Force user to end if present otherwise it will break other extensions
     active_extensions.sort(key=lambda e: e.get_name().startswith('user'))
     console.banner("Command Line")
-    print(console.green + "Active Extensions" + console.reset)
+    print(console.green + "Options" + console.reset)
+    for k, v in options.items():
+        print(" - " + console.cyan + str(k) + console.reset + ": " + console.yellow + str(v) + console.reset)
+    print(console.green + "\nActive Extensions" + console.reset)
     for e in active_extensions:
         print(" - " + console.cyan + e.get_name() + console.reset)
 
-    base_image = args.image
-
-    dig = core.DockerImageGenerator(active_extensions, args_dict, base_image)
-    exit_code = dig.build(**vars(args))
+    base_image = options["image"]
+    dig = core.DockerImageGenerator(active_extensions, options, base_image)
+    exit_code = dig.build(**options)
     if exit_code != 0:
         console.error("Build failed exiting")
         return exit_code
-    # Convert command into string
-    args.command = ' '.join(args.command)
-    return dig.run(**args_dict)
+    return dig.run(**options)
 
 
 def detect_image_os():
---
+++ test/test_yaml.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# License: BSD
+#   https://raw.githubusercontent.com/stonier/groot_rocker_extensions/devel/LICENSE
+#
+
+##############################################################################
+# Imports
+##############################################################################
+
+import unittest
+
+import groot_rocker
+import groot_rocker.console as console
+
+from . import utilities
+
+##############################################################################
+# Tests
+##############################################################################
+
+
+class ExtensionTestCase(unittest.TestCase):
+
+    def test_defaults_from_yaml(self):
+        options = groot_rocker.cli.load_arguments(
+            command_line_arguments=['-c', 'config.yaml']
+        )
+        expected_options = {
+            "image": "ubuntu:18.04",
+            "name": "foo",
+            "tag": "groot:foo",
+            "command": "/bin/bash --login -i"
+        }
+        print("\n")
+        for k, v in expected_options.items():
+            utilities.assert_details(text=k, expected=v, result=options[k])
+            self.assertEqual(options[k], v)
